c77db12753dc831b7ac5dc2199475846d3b8886e
Defernus <dehucka98@gmail.com>
2023-01-21 17:53:18 +0300

    menu pos

--- src/plugins/main_menu/systems/mod.rs
+++ src/plugins/main_menu/systems/mod.rs
@@ -1,6 +1,7 @@
 use crate::states::game_state::GameState;
 use bevy::{app::AppExit, prelude::*};
 use bevy_egui::{egui, EguiContext};
+use bevy_inspector_egui::egui::Pos2;
 
 pub mod new_game;
 
@@ -9,24 +10,34 @@ pub fn main_menu(
     mut exit: EventWriter<AppExit>,
     mut game_state: ResMut<State<GameState>>,
     mut egui_context: ResMut<EguiContext>,
+    window: Res<Windows>,
 ) {
-    egui::Window::new("Main menu").show(egui_context.ctx_mut(), |ui| {
-        ui.vertical_centered(|ui| {
-            if ui.button("Start new game").clicked() {
-                game_state
-                    .set(GameState::MenuNewGame)
-                    .expect("Failed to set game state")
-            }
+    let window = window.get_primary().expect("Failed to get primary window");
+    let w = window.width();
+    let h = window.height();
 
-            if ui.button("Load game").clicked() {
-                game_state
-                    .set(GameState::WorldLoading)
-                    .expect("Failed to set game state")
-            }
+    let pos = Pos2::new(w * 0.5 - 200.0, h * 0.5 - 100.0);
 
-            if ui.button("Exit").clicked() {
-                exit.send(AppExit);
-            }
+    egui::Window::new("Main menu")
+        .collapsible(false)
+        .default_pos(pos)
+        .show(egui_context.ctx_mut(), |ui| {
+            ui.vertical_centered(|ui| {
+                if ui.button("Start new game").clicked() {
+                    game_state
+                        .set(GameState::MenuNewGame)
+                        .expect("Failed to set game state")
+                }
+
+                if ui.button("Load game").clicked() {
+                    game_state
+                        .set(GameState::WorldLoading)
+                        .expect("Failed to set game state")
+                }
+
+                if ui.button("Exit").clicked() {
+                    exit.send(AppExit);
+                }
+            });
         });
-    });
 }
--- src/plugins/main_menu/systems/new_game.rs
+++ src/plugins/main_menu/systems/new_game.rs
@@ -6,6 +6,7 @@ use crate::{
 };
 use bevy::prelude::*;
 use bevy_egui::{egui, EguiContext};
+use bevy_inspector_egui::egui::Pos2;
 
 pub fn init_new_game(mut game_world_meta: ResMut<GameWorldMeta>) {
     game_world_meta.reset();
@@ -28,29 +29,39 @@ pub fn new_game(
     mut game_world_meta: ResMut<GameWorldMeta>,
     mut egui_context: ResMut<EguiContext>,
     mut local_state: Local<NewGameLocalState>,
+    window: Res<Windows>,
 ) {
-    egui::Window::new("Not implemented").show(egui_context.ctx_mut(), |ui| {
-        ui.horizontal(|ui| {
-            ui.label("Name: ");
-            ui.text_edit_singleline(&mut game_world_meta.name);
-        });
-        ui.horizontal(|ui| {
-            ui.label("Seed: ");
-            ui.text_edit_singleline(&mut local_state.seed);
+    let window = window.get_primary().expect("Failed to get primary window");
+    let w = window.width();
+    let h = window.height();
 
-            if ui.button("random").clicked() {
-                local_state.seed = rand::random::<u64>().to_string();
+    let pos = Pos2::new(w * 0.5 - 200.0, h * 0.5 - 100.0);
+
+    egui::Window::new("Not implemented")
+        .collapsible(false)
+        .default_pos(pos)
+        .show(egui_context.ctx_mut(), |ui| {
+            ui.horizontal(|ui| {
+                ui.label("Name: ");
+                ui.text_edit_singleline(&mut game_world_meta.name);
+            });
+            ui.horizontal(|ui| {
+                ui.label("Seed: ");
+                ui.text_edit_singleline(&mut local_state.seed);
+
+                if ui.button("random").clicked() {
+                    local_state.seed = rand::random::<u64>().to_string();
+                }
+
+                game_world_meta.seed = local_state.seed.parse().unwrap_or_else(|_| {
+                    let mut hasher = std::collections::hash_map::DefaultHasher::new();
+                    local_state.seed.hash(&mut hasher);
+                    hasher.finish()
+                }) as WorldSeed;
+            });
+
+            if ui.button("Generate world").clicked() {
+                game_state.set(GameState::WorldCreating).unwrap();
             }
-
-            game_world_meta.seed = local_state.seed.parse().unwrap_or_else(|_| {
-                let mut hasher = std::collections::hash_map::DefaultHasher::new();
-                local_state.seed.hash(&mut hasher);
-                hasher.finish()
-            }) as WorldSeed;
         });
-
-        if ui.button("Generate world").clicked() {
-            game_state.set(GameState::WorldCreating).unwrap();
-        }
-    });
 }
