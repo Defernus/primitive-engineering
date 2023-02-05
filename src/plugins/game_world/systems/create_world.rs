8f097425fa319df572bebee2cbc584e4421436f5
Defernus <dehucka98@gmail.com>
2023-02-05 22:29:50 +0300

    rm chunks creation on world loading

--- src/plugins/game_world/systems/create_world.rs
+++ src/plugins/game_world/systems/create_world.rs
@@ -1,49 +1,8 @@
-use crate::{
-    internal::{
-        chunks::{Chunk, ChunkPointer},
-        pos::{ChunkPos, VoxelPos},
-    },
-    plugins::{
-        chunks::helpers::spawn_chunk, game_world::resources::GameWorld,
-        loading::resources::GameAssets, world_generator::resources::WorldGenerator,
-    },
-    states::game_state::GameState,
-};
+use crate::{plugins::game_world::resources::GameWorld, states::game_state::GameState};
 use bevy::prelude::*;
 
-pub fn start_world_creating(
-    mut commands: Commands,
-    gen: Res<WorldGenerator>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    assets: Res<GameAssets>,
-) {
-    let mut world = GameWorld::new();
-
-    for i in 0..8 {
-        let pos = VoxelPos::from_index(i, 2);
-        let pos = ChunkPos::new(pos.x as i64 - 1, pos.y as i64 - 1, pos.z as i64 - 1);
-        let level = 0;
-
-        let (_, biomes) = world
-            .create_chunk(pos, &gen)
-            .expect(format!("Chunk already exists at {:?}:{}", pos, level).as_str());
-
-        let mut chunk = Chunk::generate(gen.clone(), biomes.clone(), pos, level);
-        let vertices = chunk.generate_vertices(level);
-        chunk.set_need_redraw(false);
-
-        let chunk = ChunkPointer::new(chunk, pos, level);
-
-        spawn_chunk(
-            &mut commands,
-            &mut meshes,
-            &assets,
-            &mut world,
-            &gen,
-            chunk,
-            vertices,
-        );
-    }
+pub fn start_world_creating(mut commands: Commands) {
+    let world = GameWorld::new();
     commands.insert_resource(world);
 }
 
